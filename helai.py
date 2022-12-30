de76ec60c69c5d96fa727cc795b7908d0aa30131
marked9999 <jewelsytofficial@gmail.com>
2022-12-30 16:34:40 +0000

    working on more

--- helai.py
+++ helai.py
@@ -1,6 +1,7 @@
 #https://www.behindthename.com/random/random.php?gender=f&number=1&sets=1&surname=&showextra=yes&all=yes
-import pyttsx3
+import pyttsx3 
 import datetime
+import speech_recognition as sr
 engine = pyttsx3.init()
 
 
@@ -26,6 +27,31 @@ def wishme():
     time()
     speak("And the date's")
     date()
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour <= 12:
+        speak("Good Morning")
+    elif hour >= 12 and hour <= 18:
+        speak("Good afternoon")
+    elif hour >= 18 and hour <= 24:
+        speak("Good evening")
+    else:
+        speak("Good Night.")
     speak("What's up") #legit npc 
 
-wishme()
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    
+    try:
+        print("proccessing")
+        query = r.recognize_google(audio, language='en-in')
+        print(query)
+
+    except Exception as e:
+        print(e)
+        speak("what?")
+        return "None"
+    return query
