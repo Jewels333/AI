4630ed3a26003b498c94514c5ce2d906b522b601
marked9999 <jewelsytofficial@gmail.com>
2022-12-31 15:45:30 +0000

    stuf

--- terminal.py
+++ terminal.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-encryptionkey = pd.read_csv(r"<YourPathTo.csv>",
+encryptionkey = pd.read_csv(r"terminal/decodekeynew (2).csv",
                             sep=',', names=['Character', 'Byte'], header=None, skiprows=[0])
 
 df = pd.DataFrame(data=encryptionkey)
@@ -11,10 +11,46 @@ df['Byte'] = df['Byte'].astype(str)
 def split(message):
     return [char for char in message]
 
-message = 'It is a beautiful day to code something new. In fact, a day like any other,' \ 
+message = 'It is a beautiful day to code something new. In fact, a day like any other,' 
 ' with new projects and ideas. Maybe also new challenges and nerve-racking bugs. Time will tell.'
 
 message_split = split(message)
 
+def code_message():
+    coded_message = ""
+
+    for i in range(len(message_split)):
+        j = message_split[i]
+        try:
+            coded_char = encryptionkey.loc[encryptionkey['Character'] == j, 'Byte'].iloc[0]
+
+        except:
+            print('unrecognized character')
+            coded_char = '@@@'
+
+        coded_message = coded_message + coded_char
+    return coded_message
+
+def decode_message(message):
+    new_word = ''
+    decoded_message = []
+
+    for i in range(0, len(message), 2):
+        j = message[i:i + 2]
+        index_nb = df[df.eq(j).any(1)]
+
+        df2 = index_nb['Character'].tolist()
+
+        s = [str(x) for x in df2]
+        decoded_message = decoded_message + s
+
+    new_word = ''.join(decoded_message)
+
+    return new_word
+
+#coded_message_str = str(coded_message) how to use 
+#print('Your decoded message:', decode_message(coded_message_str))
+
 print('Welcome to the terminal.')
 AccessInput = input('Enter Access Key\n')
+
