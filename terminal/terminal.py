4c746c837fe8ebb81d9790889389ab2d3c0db14f
marked9999 <jewelsytofficial@gmail.com>
2023-01-01 22:21:22 +0000

    finished chat

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -75,35 +75,29 @@ if AccessInput == AccessKey:
                 else:
                     print('Canceled.')
         elif menu1 == '3':
-            import time
-            import sys
-            import socket
-
-            new_socket = socket.socket()
-            host_name = socket.gethostname()
-            s_ip = socket.gethostbyname(host_name)
-            port = 8080
-            new_socket.bind((host_name, port))
-
-            print( "Binding successful!" )
-            print("This is your IP: ", s_ip)
-
-            name = LoginName
-            new_socket.listen(1)
-            conn, add= new_socket.accept()
-            print("Received connection from ", add[0])
-            print('Connection Established. Connected From: ',add[0])    
-            client = (conn.recv(1024)).decode()
-            print(client + ' has connected.')
-            conn.send(name.encode())
             
-            while True:
-                message = input('Me : ')
-                conn.send(message.encode())
-                message = conn.recv(1024)
-                message = message.decode()
-                print(client, ':', message)
-             
+           import socket
+           import sys
+           import time
+           socket_server = socket.socket()
+           server_host = socket.gethostname()
+           ip = socket.gethostbyname(server_host)
+           sport = 8080
+           print('This is your IP address: ', ip)
+           server_host = input('Enter friend\'s IP address:')
+           name = input('Enter Friend\'s name: ')
+ 
+           socket_server.connect((server_host, sport))
+           socket_server.send(name.encode())
+           server_name = socket_server.recv(1024)
+           server_name = server_name.decode()
+ 
+           print(server_name,' has joined...')
+           while True:
+                message = (socket_server.recv(1024)).decode()
+                print(server_name, ":", message)
+                message = input("Me : ")
+                socket_server.send(message.encode())  
     else:
         print('Incorrect.')
         print(Password)
