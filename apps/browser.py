d297a50c77626ec0761d35a472e5a075c1520926
marked9999 <jewelsytofficial@gmail.com>
2023-01-05 14:59:52 +0000

    working on browser

--- apps/browser.py
+++ apps/browser.py
@@ -1,23 +1,4 @@
-import webbrowser
-import google
-print('Welcome to the Scripted Browser.')
-
-LoginUser = input('Enter username:')
-LoginPass = input('Enter password:')
-f = open(terminalFolder + accountsFolder + LoginName + ".key", "r")
-length1 = 1
-Password = (f.readline())
-PasswordLen = len(Password) - 1
-Password = Password[:PasswordLen]
-f.close()
-
-if LoginPass == Password: #* Issue resovled
-   print('Welcome to the browser.')
-   search = input('Enter search/url:')
-   urlDict =  [".www", ".com", ".gov" ".net" ".me" "."]
-   if urlDict in search:
-     webbrowser.open(search)
-   else:
-     print(search(search))
-
-   #webbrowser.open()
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel('Hello World!')
+label.show()
