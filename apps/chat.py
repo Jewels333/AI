cbf680e852b741768e79583021024f625ee00e51
marked9999 <jewelsytofficial@gmail.com>
2023-01-06 21:49:37 +0000

    testing

--- apps/chat.py
+++ apps/chat.py
@@ -1,2 +1,16 @@
-f = open('apps/chat.server')
-print()
+
+LoginName = input('Enter Username:')
+LoginPass = input('Enter Password:')
+
+terminalFolder = 'terminal/'
+accountsFolder = 'accounts/'
+
+f = open(terminalFolder + accountsFolder + LoginName + ".key", "r")
+length1 = 1
+Password = (f.readline())
+PasswordLen = len(Password) - 1
+Password = Password[:PasswordLen]
+f.close()
+
+if LoginPass == Password: 
+    
