1d792223fbe7efaa0744fee69a89dab4ed21011b
marked9999 <jewelsytofficial@gmail.com>
2023-01-19 15:43:18 +0000

    worked on learner extract()

---
+++ programs/.env/learning.py
@@ -0,0 +1,15 @@
+supportWord = 'i', 'am', 'you'
+context = ''
+def extract(str):
+    extracted = ''
+    context = ''
+    extracted = str.split()
+    wordList = extracted
+    word = ['',]
+    
+    extracted = wordList
+    print(extracted)
+
+print(extract('Hi, i am jewels'))
+    
+
