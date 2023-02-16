327b6e39ed6853b7c489b4b953ee712cf52564e2
Jewels <107947731+marked9999@users.noreply.github.com>
2023-02-15 21:00:26 -0500

    @LogiDev

---
+++ games/gng5.cs
@@ -0,0 +1,16 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    str self = ''
+    str user = ''
+    int dist = 0
+    bool hit = False
+    int reach = __
+    bool inSight = False
+    string[] charS = {"name", "name", "name", "user","granny","grandpa"}
+    string[] map = {"sewer", "petStore"} 
+    //if dist == reach || inSight == True
+    
+  }
+}
