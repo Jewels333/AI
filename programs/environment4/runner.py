c117d14633dd25511ca4881d065fa74831227706
marked9999 <jewelsytofficial@gmail.com>
2023-02-25 00:21:29 +0000

    yutubi env4

--- programs/environment4/runner.py
+++ programs/environment4/runner.py
@@ -1,3 +1,5 @@
+# Enrichment Boat Simulation
+# Jewels, Yusuf 
 def find(fp, word):
     
     with open(fp, 'r') as fp:
@@ -9,14 +11,24 @@ def find(fp, word):
                 print(word, 'string exists in file')
                 print('Line Number:', lines.index(line))
                 print('Line:', line)
+                return line
 class api:
     dataFile = 'programs/environment4/data'
     envFile = 'programs/environment4/env'
     boatFile = 'programs/environment4/boat'
     class data:
         connection = open(dataFile)
+        content = connection.read()
+        with content as data: #to READ the DATA
+            pass
     class env:
         connection = open(envFile)
+        content = connection.read()
+        with content as environment: #to CREATE the ENVIORMENT
+            pass
     class boat:
         connection = open(boatFile)
+        content = connection.read()
+        with content as boat: #to CREATE the BOAT
+            pass
     
