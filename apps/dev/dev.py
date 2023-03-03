a0da312dafdd5e3394a3cf38813c6cff621c2b2a
marked9999 <jewelsytofficial@gmail.com>
2023-03-03 12:03:34 +0000

    Creating Admin Standard Code 2, @Xn1vity I have a job for you can you enter all the charecters for the code in apps/dev/dev.py Where it says element = ''

---
+++ apps/dev/dev.py
@@ -0,0 +1,141 @@
+class tool:
+    def encode(str, type):
+        if type == 'USC-1': #User Standard Code 1
+            strLen = len(str)
+            
+        if type == 'ASC-2': #Admin Standard Code 2
+            strLen = len(str)
+            encoded = ''
+            for element in str:
+               with element as char:
+                    if char == 'a':
+                        element = ''
+                    elif char == 'b':
+                        element = ''
+                    elif char == 'c':
+                        element = ''
+                    elif char == 'd':
+                        element = ''
+                    elif char == 'e':
+                        element = ''
+                    elif char == 'f':
+                        element = ''
+                    elif char == 'g':
+                        element = ''
+                    elif char == 'h':
+                        element = ''
+                    elif char == 'i':
+                        element = ''
+                    elif char == 'j':
+                        element = ''
+                    elif char == 'k':
+                        element = ''
+                    elif char == 'l':
+                        element = ''
+                    elif char == 'm':
+                        element = ''
+                    elif char == 'n':
+                        element = ''
+                    elif char == 'o':
+                        element = ''
+                    elif char == 'p':
+                        element = ''
+                    elif char == 'q':
+                        element = ''
+                    elif char == 'r':
+                        element = ''
+                    elif char == 's':
+                        element = ''
+                    elif char == 't':
+                        element = ''
+                    elif char == 'u':
+                        element = ''
+                    elif char == 'v':
+                        element = ''
+                    elif char == 'w':
+                        element = ''
+                    elif char == 'x':
+                        element = ''
+                    elif char == 'y':
+                        element = ''
+                    elif char == 'z':
+                        element = ''
+                    if char == 'A':
+                        element = ''
+                    elif char == 'B':
+                        element = ''
+                    elif char == 'C':
+                        element = ''
+                    elif char == 'D':
+                        element = ''
+                    elif char == 'E':
+                        element = ''
+                    elif char == 'F':
+                        element = ''
+                    elif char == 'G':
+                        element = ''
+                    elif char == 'H':
+                        element = ''
+                    elif char == 'I':
+                        element = ''
+                    elif char == 'J':
+                        element = ''
+                    elif char == 'K':
+                        element = ''
+                    elif char == 'L':
+                        element = ''
+                    elif char == 'M':
+                        element = ''
+                    elif char == 'N':
+                        element = ''
+                    elif char == 'O':
+                        element = ''
+                    elif char == 'P':
+                        element = ''
+                    elif char == 'Q':
+                        element = ''
+                    elif char == 'R':
+                        element = ''
+                    elif char == 'S':
+                        element = ''
+                    elif char == 'T':
+                        element = ''
+                    elif char == 'U':
+                        element = ''
+                    elif char == 'V':
+                        element = ''
+                    elif char == 'W':
+                        element = ''
+                    elif char == 'X':
+                        element = ''
+                    elif char == 'Y':
+                        element = ''
+                    elif char == 'Z':
+                        element = ''
+                    elif char == '1':
+                        element = '' 
+                    elif char == '2':
+                        element = ''
+                    elif char == '3':
+                        element = ''
+                    elif char == '4':
+                        element = ''
+                    elif char == '5':
+                        element = ''
+                    elif char == '6':
+                        element = ''
+                    elif char == '7':
+                        element = ''
+                    elif char == '8':
+                        element = ''
+                    elif char == '9':
+                        element = ''
+                    elif char == '0':
+                        element = ''                     
+               encoded = element, end=' '
+
+                
+    def decode(str, type):
+        pass
+print('Welcome to the Developer Portal.')
+input = input('Enter your access key:')
