b82fda6c8b45d17e96a22a0f9e4032c7c814d9a8
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 12:51:49 +0000

    trying somethign

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-
 pip install flask
-pip install socket
 pip install sudo
-pip install ubuntu
-ubuntu iptables -A INPUT -i lo -j ACCEPT
-ubuntu iptables -A OUTPUT -o lo -j ACCEPT
+sudo apt-get update sudo apt-get install iptables
+iptables -I INPUT -j ACCEPT
 python -m flask run --host=0.0.0.0
