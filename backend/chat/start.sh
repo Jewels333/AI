8e49548496380697ef444f6f6bcd714baf28fe44
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 12:58:39 +0000

    rewrite i did it wrong @Jewels

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pip install flask
 pip install sudo
-apt-get update sudo apt-get install iptables
+apt-get update sudo 
+apt-get install iptables
 iptables -I INPUT -j ACCEPT
 python -m flask run --host=0.0.0.0
