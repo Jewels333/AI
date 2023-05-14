d360066beb3373659ec386005d563d82350ebfe1
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 13:05:04 +0000

    this is lidna dumb
    cring

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -2,7 +2,7 @@
 pip install flask
 pip install sudo
 pip install apt
-apt-get update
-apt-get install iptables
+python sudo apt-get update
+python sudo apt-get install iptables
 iptables -I INPUT -j ACCEPT
 python -m flask run --host=0.0.0.0
