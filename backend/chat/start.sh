84715182b0423da53a12fb272332f6eebc674dad
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 12:58:19 +0000

    apt-get update sudo

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install flask
 pip install sudo
-sudo apt-get update sudo apt-get install iptables
+apt-get update sudo apt-get install iptables
 iptables -I INPUT -j ACCEPT
 python -m flask run --host=0.0.0.0
