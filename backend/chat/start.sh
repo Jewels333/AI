31e9315cf0af3432853a7aa51002316021e989a8
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 01:56:54 +0000

    retry?

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -3,6 +3,6 @@
 pip install flask
 pip install socket
 pip install sudo
-python -m sudo iptables -A INPUT -i lo -j ACCEPT
-python -m sudo iptables -A OUTPUT -o lo -j ACCEPT
+python -m iptables -A INPUT -i lo -j ACCEPT
+python -m iptables -A OUTPUT -o lo -j ACCEPT
 python -m flask run --host=0.0.0.0
