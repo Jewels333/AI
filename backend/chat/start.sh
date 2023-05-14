5743ea0922b0919c44727ef821114603be631ab0
marked9999 <jewelsytofficial@gmail.com>
2023-05-14 12:37:43 +0000

    co,,it 283782

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -3,6 +3,7 @@
 pip install flask
 pip install socket
 pip install sudo
-python -m iptables -A INPUT -i lo -j ACCEPT
-python -m iptables -A OUTPUT -o lo -j ACCEPT
+pip install ubuntu
+ubuntu iptables -A INPUT -i lo -j ACCEPT
+ubuntu iptables -A OUTPUT -o lo -j ACCEPT
 python -m flask run --host=0.0.0.0
