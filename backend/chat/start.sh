effa06a40fe972a73ad3377b0fff2d471f95bef8
marked9999 <jewelsytofficial@gmail.com>
2023-05-15 12:00:19 +0000

    rewrite start.sh

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-pip install flask
-pip install sentry-sdk[flask]
-#pip install sudo
-#pip install apt
-#python sudo apt-get update
-#python sudo apt-get install iptables
-#iptables -I INPUT -j ACCEPT
 flask --app app run --host=0.0.0.0
