195097a4bcab912687beaab122ac72c72e8bb899
marked9999 <jewelsytofficial@gmail.com>
2023-05-15 11:00:58 +0000

    trying something

--- backend/chat/start.sh
+++ backend/chat/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 pip install flask
+pip install sentry-sdk[flask]
 #pip install sudo
 #pip install apt
 #python sudo apt-get update
 #python sudo apt-get install iptables
 #iptables -I INPUT -j ACCEPT
-flask --app app run
+flask --app app run --host=0.0.0.0
